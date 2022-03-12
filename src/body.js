6a4431f048eb9703808fb274f7be401c11d7589a
Nick Carducci <nmcarducci@gmail.com>
2022-03-12 14:50:38 -0500

    Update body.js

--- src/body.js
+++ src/body.js
@@ -230,11 +230,16 @@ class Body extends React.Component {
             jobs, variable-part of m+l sprint begs for.
             <br />
             <br />
-            you miss the boat, and if everyone is on the boat, it would be too
-            broad and descriptive. accrue doesn't make money for the plunderer
-            is laborless demand, yet without fractional reserves of transaction
-            fee based security depositary receipt disruptive technology over
-            monetary for{space}
+            you miss the boat, and if everyone is on the boat, it would be
+            {space}
+            <span style={{ color: "slateblue" }}>
+              too broad and descriptive
+            </span>
+            , as to surrender the resources of others, as not to compete with
+            producers for their favor yet trade, not premium, yet. accrue
+            doesn't make money for the plunderer is laborless demand, yet
+            without fractional reserves of transaction fee based security
+            depositary receipt disruptive technology over monetary for{space}
             <span style={{ color: "slateblue" }}>
               less hours per expense growth
             </span>
