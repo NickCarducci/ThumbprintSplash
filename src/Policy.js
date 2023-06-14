766a346a6bad8f8719ae433acc6316c5f1b07eca
Nick Carducci <nmcarducci@gmail.com>
2023-06-14 11:13:40 -0400

    Update Policy.js

--- src/Policy.js
+++ src/Policy.js
@@ -1,5 +1,169 @@
 import React from "react";
 
+class Fumbler extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        {" "}
+        <div
+          onClick={() => this.setState({ convicted: !this.state.convicted })}
+          style={{
+            padding: "10px",
+            display: "flex",
+            justifyContent: "center",
+            width: "calc(100% - 20px)"
+          }}
+        >
+          (View as {this.state.convicted ? "Unconvicted" : "Convicted"})
+        </div>
+        <div
+          style={{
+            color: !this.state.convicted
+              ? "rgb(240,240,180)"
+              : "rgb(240,240,180)",
+            padding: "15px",
+            marginTop: "10px",
+            width: "calc(90% - 30px)",
+            flexDirection: "column",
+            display: "inline-block",
+            alignItems: "center",
+            textAlign: "center",
+            backgroundColor: !this.state.convicted
+              ? "rgba(150,150,250,.6)"
+              : "rgba(150,250,250,.6)"
+          }}
+        >
+          {this.state.convicted &&
+            "Are you on a private computer? Firebase chat uses cache to lessen the download"}
+          {/*Firebase chat uses cache to lessen the (down)load
+      <br />*/}
+          {!this.state.convicted && (
+            <div>
+              <span>
+                <i
+                  className="fas fa-key"
+                  style={{
+                    margin: "1px",
+                    zIndex: "7",
+                    position: "absolute",
+                    color: "rgb(150,150,250)"
+                  }}
+                ></i>{" "}
+                <i className="fas fa-box" style={{ fontSize: "20px" }}></i>
+              </span>{" "}
+              for your first <i className="fas fa-laptop"></i>{" "}
+              <i className="fas fa-tablet-alt"></i>
+              <br />{" "}
+              <div style={{ display: "flex" }}>
+                {this.state.informationAbout ? (
+                  <div style={{ display: "inline-block" }}>
+                    While Firestore data is encrypted by Google in transit &
+                    their cloud, it is&nbsp;
+                    <span
+                      style={{
+                        color: "red"
+                      }}
+                    >
+                      unencrypted
+                    </span>
+                    &nbsp;in your browser's cache.
+                  </div>
+                ) : (
+                  <div style={{ display: "inline-block" }}>
+                    End-to-end, on-device, encryption should be fashioned on
+                    the&nbsp;
+                    <span
+                      style={{
+                        color: "red"
+                      }}
+                    >
+                      private devices
+                    </span>
+                    &nbsp;you would like to hold your keys.
+                  </div>
+                )}
+                <div
+                  style={{
+                    color: "white",
+                    padding: "9px",
+                    paddingTop: "2px",
+                    borderRadius: "15px",
+                    border: "1px solid",
+                    height: "10px"
+                  }}
+                  onClick={() =>
+                    this.setState({
+                      informationAbout: !this.state.informationAbout
+                    })
+                  }
+                >
+                  i
+                </div>
+              </div>
+              <br />
+              Your&nbsp;
+              <span
+                style={{
+                  color: "red"
+                }}
+              >
+                prime-number-key
+              </span>
+              &nbsp;can decipher jumbled messages & media (
+              <span
+                style={{
+                  color: "rgb(150,250,250)"
+                }}
+              >
+                if non-convict, see nationalsecuritycasino.com
+              </span>
+              ) and share this ability with other devices. Chats are encrypted
+              with their own&nbsp;
+              <span style={{ color: "rgb(200,210,220)" }}>
+                <span style={{ textDecoration: "underline" }}>
+                  keyboxes per threadId
+                </span>{" "}
+                [recipients, entityCollection+doc.id]
+              </span>
+              ,{" "}
+              <span
+                style={{
+                  color: "rgb(130,120,210)"
+                }}
+              >
+                Devices will have to be re-forged by revisiting an&nbsp;
+                <span
+                  style={{
+                    color: "red"
+                  }}
+                >
+                  active
+                </span>
+                &nbsp;device.
+              </span>{" "}
+              If you cannot visit an active device your messages and media will
+              likely be unreadable forever.
+              <br />
+              <br />
+            </div>
+          )}
+          <div
+            style={{
+              backgroundColor: "rgba(255,255,255,.8)",
+              color: "grey"
+            }}
+          >
+            NFC is privacy first product map
+            <br />
+            Removes liability of private code, if only machine readable
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Policy extends React.Component {
   state = {};
   render() {
@@ -120,6 +284,7 @@ class Policy extends React.Component {
             <br />
             <br />
           </div>
+          <Fumbler />
           <div
             style={{
               height: "min-content",
@@ -151,166 +316,12 @@ class Policy extends React.Component {
             encrypted in transit, stored on encrypted disks on the servers, and
             may be stored in your browser's cache.{" "}
             <b>
-              So, please only open your chats on a private device. <br />
+              So, please only open your chats on a private device (with our SSL
+              HTTPS host certifications). <br />
               <br />
               Data is otherwise only controlled by you, granting that it is
-              deemed by us to be appropriate for all audiences
+              deemed by us to be appropriate for all audiences (deepai)
             </b>
-            <div
-              onClick={() =>
-                this.setState({ convicted: !this.state.convicted })
-              }
-              style={{
-                padding: "10px",
-                display: "flex",
-                justifyContent: "center",
-                width: "calc(100% - 20px)"
-              }}
-            >
-              (View as {this.state.convicted ? "Unconvicted" : "Convicted"})
-            </div>
-            <div
-              style={{
-                color: !this.state.convicted
-                  ? "rgb(240,240,180)"
-                  : "rgb(240,240,180)",
-                padding: "15px",
-                marginTop: "10px",
-                width: "calc(90% - 30px)",
-                flexDirection: "column",
-                display: "inline-block",
-                alignItems: "center",
-                textAlign: "center",
-                backgroundColor: !this.state.convicted
-                  ? "rgba(150,150,250,.6)"
-                  : "rgba(150,250,250,.6)"
-              }}
-            >
-              {this.state.convicted &&
-                "Are you on a private computer? Firebase chat uses cache to lessen the download"}
-              {/*Firebase chat uses cache to lessen the (down)load
-              <br />*/}
-              {!this.state.convicted && (
-                <div>
-                  <span>
-                    <i
-                      className="fas fa-key"
-                      style={{
-                        margin: "1px",
-                        zIndex: "7",
-                        position: "absolute",
-                        color: "rgb(150,150,250)"
-                      }}
-                    ></i>{" "}
-                    <i className="fas fa-box" style={{ fontSize: "20px" }}></i>
-                  </span>{" "}
-                  for your first <i className="fas fa-laptop"></i>{" "}
-                  <i className="fas fa-tablet-alt"></i>
-                  <br />{" "}
-                  <div style={{ display: "flex" }}>
-                    {this.state.informationAbout ? (
-                      <div style={{ display: "inline-block" }}>
-                        While Firestore data is encrypted by Google in transit &
-                        their cloud, it is&nbsp;
-                        <span
-                          style={{
-                            color: "red"
-                          }}
-                        >
-                          unencrypted
-                        </span>
-                        &nbsp;in your browser's cache.
-                      </div>
-                    ) : (
-                      <div style={{ display: "inline-block" }}>
-                        End-to-end, on-device, encryption should be fashioned on
-                        the&nbsp;
-                        <span
-                          style={{
-                            color: "red"
-                          }}
-                        >
-                          private devices
-                        </span>
-                        &nbsp;you would like to hold your keys.
-                      </div>
-                    )}
-                    <div
-                      style={{
-                        color: "white",
-                        padding: "9px",
-                        paddingTop: "2px",
-                        borderRadius: "15px",
-                        border: "1px solid",
-                        height: "10px"
-                      }}
-                      onClick={() =>
-                        this.setState({
-                          informationAbout: !this.state.informationAbout
-                        })
-                      }
-                    >
-                      i
-                    </div>
-                  </div>
-                  <br />
-                  Your&nbsp;
-                  <span
-                    style={{
-                      color: "red"
-                    }}
-                  >
-                    prime-number-key
-                  </span>
-                  &nbsp;can decipher jumbled messages & media (
-                  <span
-                    style={{
-                      color: "rgb(150,250,250)"
-                    }}
-                  >
-                    if non-convict, see nationalsecuritycasino.com
-                  </span>
-                  ) and share this ability with other devices. Chats are
-                  encrypted with their own&nbsp;
-                  <span style={{ color: "rgb(200,210,220)" }}>
-                    <span style={{ textDecoration: "underline" }}>
-                      keyboxes per threadId
-                    </span>{" "}
-                    [recipients, entityCollection+doc.id]
-                  </span>
-                  ,{" "}
-                  <span
-                    style={{
-                      color: "rgb(130,120,210)"
-                    }}
-                  >
-                    Devices will have to be re-forged by revisiting an&nbsp;
-                    <span
-                      style={{
-                        color: "red"
-                      }}
-                    >
-                      active
-                    </span>
-                    &nbsp;device.
-                  </span>{" "}
-                  If you cannot visit an active device your messages and media
-                  will likely be unreadable forever.
-                  <br />
-                  <br />
-                </div>
-              )}
-              <div
-                style={{
-                  backgroundColor: "rgba(255,255,255,.8)",
-                  color: "grey"
-                }}
-              >
-                NFC is privacy first product map
-                <br />
-                Removes liability of private code, if only machine readable
-              </div>
-            </div>
             <br />
             <br />
           </div>
