8b7ccffeb96fc3d8a8126bebd26e98efb644454f
Nick Carducci <nmcarducci@gmail.com>
2024-10-29 14:06:35 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 import NavBar from "./navbar.js";
 import Ourusers from "./ourusers.js";
 import Messageus from "./messageus.js";
@@ -17,53 +19,58 @@ class App extends React.Component {
   }
   render() {
     const { see2017 } = this.state;
+    const responsive = {
+      superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5,
+      },
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+      },
+    };
     return (
       <div style={{ width: "100%", height: "min-content" }}>
-        <div
-          style={{
-            textAlign: "center",
-          }}
-        >
-          <h4>
-            Find things to do around you.
-            <br />
-            Keep track of plans with friends.
-          </h4>
-          "I want to democratize budget proposals and elections for student
-          government associations, municipality governments, and corporate
-          governance boards."
-          <br />- Nick Carducci (CEO, Founder)
-          <div
-            style={{
-              justifyContent: "space-between",
-              width: "190px",
-              display: "flex",
-            }}
-          >
+        <Carousel autoPlay={true} infiniteLoop={true} interval={5000}>
+          <div>
+            "I want to democratize budget proposals and elections for student
+            government associations, municipality governments, and corporate
+            governance boards."
+            <br />- Nick Carducci (CEO, Founder)
+          </div>
+          <div>
+            <h2>Find things to do around you.</h2>
             <a href="https://cityscan.org" style={{ color: "black" }}>
-              Explorer
               <img
                 style={{ height: "86px", width: "86px" }}
                 src="https://www.dl.dropboxusercontent.com/s/cp1vzypyfh1sb03/Thumbprint%20corp%20logo%20-%20colors_round%20%282%29.png?dl=0"
                 //<img src="https://www.dl.dropboxusercontent.com/s/9oci4efa4zsh90q/Thumbprint_logo.png?dl=0" />
                 alt="err"
               />
-              <br />
-              cityscan.org
             </a>
+            <br />
+          </div>
+          <div>
+            <h2>Keep track of plans with friends.</h2>
             <a href="https://thumbprint.app" style={{ color: "black" }}>
-              Calendar
               <img
                 style={{ height: "86px", width: "86px" }}
                 src="https://www.dl.dropboxusercontent.com/s/9oci4efa4zsh90q/unnamed.png?dl=0"
                 alt="err"
               />
-              <br />
-              thumbprint.app
             </a>
+            <br />
           </div>
-          Approval Condorcet voting, higher frequency polls and civic engagement
-        </div>
+        </Carousel>
         {this.state.openBudget ? (
           <USBudget width={this.props.width} />
         ) : this.state.openThumbprint ? (
